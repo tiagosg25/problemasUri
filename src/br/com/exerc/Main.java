cb6b1dfdc14d01a864b1ee7e1bfc1782d3dfc9e8
tiagosg <tiagosg@algartelecom.com.br>
2018-12-13 09:04:46 -0200

    Adicionando nova mensagem e comentario.

--- src/br/com/exerc/Main.java
+++ src/br/com/exerc/Main.java
@@ -5,5 +5,7 @@ import javax.swing.*;
 public class Main {
     public static void main(String ...args){
         JOptionPane.showMessageDialog(null,"Isso é apenas um teste.");
+        System.out.println("Isso tambem é um teste.");
+        //Isso é um comentario teste.
     }
 }
