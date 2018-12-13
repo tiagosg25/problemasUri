4bc4b8a2a84c4900abb11945a5aae5baedef8ba3
tiagosg <tiagosg@algartelecom.com.br>
2018-12-13 08:50:12 -0200

    Nova msg de teste

--- src/br/com/exerc/Main.java
+++ src/br/com/exerc/Main.java
@@ -1,7 +1,9 @@
 package br.com.exerc;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String ...args){
-
+        JOptionPane.showMessageDialog(null,"Isso é apenas um teste.");
     }
 }
